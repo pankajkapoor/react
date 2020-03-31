a7641ba3d553b04b5681d25696c49c10a9fc780a
Pankaj Kapoor <pankaj26293@gmail.com>
2020-03-31 17:51:02 +0530

    Advance topics

--- src/App.js
+++ src/App.js
@@ -1,75 +1,33 @@
 import React, { Component } from "react";
-import NavBar from "./components/navbar";
-import Counters from "./components/counters";
-import "./App.css";
+import MoviePage from "./context/MoviePage";
+import UserContext from "./context/userContext";
+import CartContext from "./context/cartContext";
+import Login from "./context/Login";
 
 class App extends Component {
-  constructor() {
-    super();
-    console.log(`App  - constructor`);
-    this.state.something =
-      "States can be defined here this is the right place to do so";
-  }
-
-  componentDidMount() {
-    // Ajax call
-    console.log("App - Mounted");
-  }
-
-  state = {
-    counters: [
-      { id: 1, value: 1 },
-      { id: 2, value: 5 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
+  handleLoggedIn = username => {
+    console.log("Getting the user: " + username);
+    const user = { name: "Mosh" };
+    this.setState({ currentUser: user });
   };
 
-  handleDelete = counter => {
-    let { counters } = this.state;
-    counters = counters.filter(item => item.id !== counter.id);
-    this.setState({ counters });
-  };
-
-  handleIncrement = counter => {
-    const counters = this.state.counters;
-    let index = counters.indexOf(counter);
-    counters[index].value = counters[index].value + 1;
-    this.setState({ counters });
-  };
-
-  handleDecrement = counter => {
-    const counters = this.state.counters;
-    let index = counters.indexOf(counter);
-    counters[index].value = counters[index].value - 1;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters });
-  };
+  state = { currentUser: null };
 
   render() {
-    console.log("App - Rendered");
     return (
-      <React.Fragment>
-        <NavBar
-          totalCounters={this.state.counters.filter(v => v.value > 0).length}
-        />
-        <main role="main" className="container">
-          <Counters
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onReset={this.handleReset}
-            counters={this.state.counters}
-          />
-        </main>
-      </React.Fragment>
+      <CartContext.Provider value={{ cart: [] }}>
+        <UserContext.Provider
+          value={{
+            currentUser: this.state.currentUser,
+            onLoggedIn: this.handleLoggedIn
+          }}
+        >
+          <div>
+            <MoviePage />
+            <Login />
+          </div>
+        </UserContext.Provider>
+      </CartContext.Provider>
     );
   }
 }
